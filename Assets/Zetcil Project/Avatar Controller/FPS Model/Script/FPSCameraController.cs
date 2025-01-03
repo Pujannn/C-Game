1188e91d04c7d843a23da36c386b9b9cb618f0e1
Pujannn <fauzanfredi@gmail.com>
2025-01-03 17:35:45 +0700

    Initial commit
    C Game 3D

---
+++ Assets/Zetcil Project/Avatar Controller/FPS Model/Script/FPSCameraController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Zetcil
+{
+
+    public class FPSCameraController : MonoBehaviour
+    {
+        [Header("Main Settings")]
+        public Camera TargetCamera;
+        public float Sensitivity;
+
+        [Header("Mouse Settings")]
+        public KeyCode MouseButton = KeyCode.Mouse1;
+        public bool CursorVisible;
+        public bool CursorLocked;
+        private float X;
+        private float Y;
+
+        RaycastHit RaycastResult;
+
+        bool MouseLookActive = true;
+
+        public static bool IsPointerOverUIElement()
+        {
+            return IsPointerOverUIElement(GetEventSystemRaycastResults());
+        }
+        ///Returns 'true' if we touched or hovering on Unity UI element.
+        public static bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+        {
+            for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+            {
+                RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+                if (curRaysastResult.gameObject.layer == LayerMask.NameToLayer("UI"))
+                    return true;
+            }
+            return false;
+        }
+        ///Gets all event systen raycast results of current mouse or touch position.
+        static List<RaycastResult> GetEventSystemRaycastResults()
+        {
+            PointerEventData eventData = new PointerEventData(EventSystem.current);
+            eventData.position = Input.mousePosition;
+            List<RaycastResult> raysastResults = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, raysastResults);
+            return raysastResults;
+        }
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.visible = CursorVisible;
+            Cursor.lockState = CursorLockMode.None;
+            if (CursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            Vector3 euler = TargetCamera.transform.rotation.eulerAngles;
+            X = euler.x;
+            Y = euler.y;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.LeftAlt))
+            {
+                CursorVisible = true;
+                Cursor.visible = true;
+                MouseLookActive = false;
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            if (Input.GetKey(KeyCode.Mouse0) && !IsPointerOverUIElement())
+            {
+                MouseLookActive = true;
+                if (CursorLocked)
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+            }
+
+            if ((Input.GetKey(MouseButton) || MouseButton == KeyCode.None) && MouseLookActive)
+            {
+                const float minX = 0.0f;
+                const float maxX = 360.0f;
+                const float minY = -90.0f;
+                const float maxY = 90.0f;
+
+                X += Input.GetAxis("Mouse X") * (Sensitivity * Time.deltaTime);
+                if (X < minX) X += maxX;
+                else if (X > maxX) X -= maxX;
+                Y -= Input.GetAxis("Mouse Y") * (Sensitivity * Time.deltaTime);
+                if (Y < minY) Y = minY;
+                else if (Y > maxY) Y = maxY;
+
+                TargetCamera.transform.rotation = Quaternion.Euler(Y, X, 0.0f);
+            }
+        }
+    }
+}
---
+++ Assets/Zetcil Project/Avatar Controller/FPS Model/Script/FPSMovementController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zetcil
+{
+
+    public class FPSMovementController : MonoBehaviour
+    {
+
+        [Header("Main Settings")]
+        public CharacterController TargetPlayer;
+        public Camera TargetCamera;
+
+        [Header("Animation Settings")]
+        public bool usingAnimation;
+        public Animator TargetAnimator;
+        public KeyCode ReloadButton = KeyCode.R;
+
+        [Header("Movement Settings")]
+        public float PlayerSpeed;
+        public float JumpSpeed;
+        public float Gravity;
+        Vector3 moveDirection;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (TargetPlayer.isGrounded)
+            {
+
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                moveDirection = TargetCamera.transform.TransformDirection(moveDirection);
+                moveDirection *= PlayerSpeed;
+
+                if (Input.GetButton("Jump"))
+                    moveDirection.y = JumpSpeed;
+
+            }
+            moveDirection.y -= Gravity * Time.deltaTime;
+            TargetPlayer.Move(moveDirection * Time.deltaTime);
+
+            if (usingAnimation)
+            {
+                if (moveDirection.x > 0 || moveDirection.z > 0)
+                {
+                    TargetAnimator.SetFloat("Speed", 2);
+                } else
+                {
+                    TargetAnimator.SetFloat("Speed", -2);
+                }
+                if (Input.GetKeyDown(ReloadButton))
+                {
+                    TargetAnimator.SetTrigger("Reload");
+                }
+            }
+
+        }
+    }
+}
